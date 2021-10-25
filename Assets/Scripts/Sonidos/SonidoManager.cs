23363ea27e9c5f08e8655bcbf902d5ff3adf9b42
CrossFF <nahuelgt98@gmail.com>
2021-10-25 12:55:50 -0300

    lunes 12 55

---
+++ Assets/Scripts/Sonidos/SonidoManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonidoManager : MonoBehaviour
+{
+    [SerializeField]private List<AudioSource> _audioGUI;
+    [SerializeField]private List<AudioSource> _audioAmbiente;
+    [SerializeField]private List<AudioSource> _audioMusica;
+}

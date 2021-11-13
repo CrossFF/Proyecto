4a528112d4c610fa927d4fa340b4f93558eb7979
CrossFF <nahuelgt98@gmail.com>
2021-11-12 21:34:21 -0300

    entrega

--- Assets/Scripts/Mecha/SystemUIFuncionamiento.cs
+++ Assets/Scripts/Mecha/SystemUIFuncionamiento.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SystemUIFuncionamiento : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
@@ -10,21 +11,25 @@ public class SystemUIFuncionamiento : MonoBehaviour, IPointerEnterHandler, IPoin
     public void OnPointerDown(PointerEventData eventData)
     {
         //si hay un sistema asignado
-        if(system != null)
+        if (system != null)
         {
             // activo el menu de gestion de energia
-            GameObject.Find("Panel_Funcionamiento").GetComponent<UIFuncionamiento>().GestionarEnergia(system);  
+            GameObject.Find("Panel_Funcionamiento").GetComponent<UIFuncionamiento>().GestionarEnergia(system);
         }
-        
+
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        // resalto el cuadrado
+        if (system != null)
+            // resalto el cuadrado
+            GetComponent<Image>().color = Color.gray;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        // desresalto el cuadro
+        if (system != null)
+            // desresalto el cuadro
+            GetComponent<Image>().color = Color.white;
     }
 }
